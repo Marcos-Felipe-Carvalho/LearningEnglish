962d172db0a1a9fe58b2cc95b82268893bb8e178
Marcos Felipe Carvalho <marcosfelipec2001@gmail.com>
2020-12-08 18:52:18 -0300

    Implementada a lógica de atualizar rodada

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -11,26 +11,50 @@ export class PanelComponent implements OnInit {
 
   public phrases: Array<Phrase> = PHRASES
   public instruction: string = "Traduza a frase:"
-  public response:string
+  public response: string = ''
 
-  public round:number = 0
+  public round: number = 0
   public roundPhrase: Phrase
 
+  //Progress
+  public progress: number = 0
 
-  constructor() { 
-    this.roundPhrase = this.phrases[this.round]
-    console.log(this.roundPhrase)
+  constructor() {
+    this.updatedRound()
   }
 
   ngOnInit(): void {
   }
 
-  public updateResponse(response:Event): void{
+  public updateResponse(response: Event): void {
     this.response = (<HTMLInputElement>response.target).value
-    //console.log(this.response)
   }
 
-  public checkAnswer():void{
-    console.log("Verificar resposta: ", this.response)
+  public checkAnswer(): void {
+
+    if (this.roundPhrase.phrasePtBr == this.response) {
+      //alternar pergunta da rodada this.round++
+      console.log(this.round)
+
+      this.round++
+
+      //atualiza o objeto roundPhrase
+      this.updatedRound()
+
+      //atualiza a barra de progresso
+      this.progress = this.progress + (100 / (this.phrases.length))
+
+
+    } else {
+      alert('Tradução errada')
+    }
+
+  }
+
+  public updatedRound():void{
+    //define a rodada da frase
+    this.roundPhrase = this.phrases[this.round]
+     //limpar resposta do usuário
+     this.response = ''
   }
 }
--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent implements OnInit {
+
+  //A variável progress rece parâmetros da classe Panel
+  @Input() public progress: number = 0
 
   constructor() { }
 
